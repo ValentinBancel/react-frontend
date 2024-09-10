e58d5bb50a630bc3e566c8d51e9157d52f52ea0d
Lykkss <quaglierilisa3@gmail.com>
2024-09-10 17:59:44 +0200

    change image accueiltemplate

--- src/components/TemplateAccueil.js
+++ src/components/TemplateAccueil.js
@@ -16,7 +16,7 @@ const TemplateAccueil = () => {
     return (
         <>
             {/* Section d'accueil avec une image de fond */}
-            <section className="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat">
+            <section className="relative bg-[url(https://images.unsplash.com/photo-1527529482837-4698179dc6ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat">
                 <div className="absolute inset-0 bg-gray-900/75 sm:bg-transparent sm:from-gray-900/95 sm:to-gray-900/25"></div>
                 <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
                     <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
