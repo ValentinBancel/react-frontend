26a104b99020f684b16ff636dafaed017a3fde2c
Lykkss <quaglierilisa3@gmail.com>
2025-02-10 22:11:12 +0100

    update react-scripts version to use caret notation and remove unused sections in FestivalTemplate

--- src/components/FestivalTemplate.js
+++ src/components/FestivalTemplate.js
@@ -33,21 +33,6 @@ function FestivalTemplate() {
         </div>
       </section>
 
-      {/* Section Actualités */}
-      <section className="py-12 bg-white">
-        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
-          <h2 className="text-3xl font-bold sm:text-4xl mb-6">Actualités</h2>
-          <NewsSection />
-        </div>
-      </section>
-
-      {/* Section Programme */}
-      <section id="programme" className="py-12 bg-gray-50">
-        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
-          <h2 className="text-3xl font-bold sm:text-4xl mb-6">Programme</h2>
-          <Programme />
-        </div>
-      </section>
 
       {/* Section Carte des lieux */}
       <section className="py-12 bg-white">
@@ -56,14 +41,6 @@ function FestivalTemplate() {
           <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
             <MapComponent />
           </div>
-        </div>
-      </section>
-
-      {/* Section FAQ */}
-      <section id="faq" className="py-12 bg-gray-100">
-        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
-          <h2 className="text-3xl font-bold sm:text-4xl mb-6">FAQ</h2>
-          <FAQ />
         </div>
       </section>
     </div>
