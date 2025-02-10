d3173d6a67a470e444c0699f79644d3ea1a02d1c
Lykkss <quaglierilisa3@gmail.com>
2025-02-10 22:31:11 +0100

    refactor: remove unused imports in FestivalTemplate component

--- src/components/FestivalTemplate.js
+++ src/components/FestivalTemplate.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import MapComponent from './MapComponent';
-import Programme from './Programme';
-import FAQ from './faq';
-import NewsSection from './NewsSection';
 import eventTicketImage from '../assets/melissa-askew-AUXanrckXn0-unsplash.jpg';
 
 function FestivalTemplate() {
